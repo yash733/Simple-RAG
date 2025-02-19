b079bb7fb6db2a39e83e80f5e49957c6524071db
yash733 <yash733622@gmail.com>
2025-02-20 00:30:46 +0530

    'Finished'

--- rag_dynamic.py
+++ rag_dynamic.py
@@ -1,17 +1,28 @@
 import os
 from dotenv import load_dotenv
 import streamlit as st
+from streamlit_pdf_viewer import pdf_viewer
+
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
-from streamlit_pdf_viewer import pdf_viewer
+from langchain_core.prompts import ChatPromptTemplate
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain.chains import create_retrieval_chain
 
 # Load API Keys
 load_dotenv()
 os.environ["LANGCHAIN_API_KEY"] = os.getenv("LANGCHAIN_API_KEY")
 os.environ["LANGCHAIN_TRACKING_V2"] = "true"
 os.environ["GROQ_API_KEY"] = os.getenv("GROQ_API_KEY")
+os.environ['LANGCHAIN_PROJECT'] = os.getenv('LANGCHAIN_PROJECT')
+
+from langchain_groq import ChatGroq
+
+model = ChatGroq(model="llama3-70b-8192")
+
+
 
 st.title("📄 RAG - Why You Should Hire Me!")
 
@@ -37,16 +48,32 @@ if uploaded_file:
 
     #from langchain_community.vectorstores import FAISS 
     vectorstore = FAISS.from_documents(docs, embedding_function)
-    #vectorstore = FAISS.from_texts(texts, embedding_function)      !! if loading text !!
+    #__ vectorstore = FAISS.from_texts(texts, embedding_function)      !! if loading text !!
+    retriever = vectorstore.as_retriever()
 
+    #from langchian_core.prompts import ChatPromptTemplate
+    prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system","Act like my assistant, use the provided context and answer the question's asked by recruter. Specify my capable in number. Also any other details that can help me in getting shortlisted in the role of Machine Learning Engineer, Artificial Intelligance Engineer, Data Analytics, Data Scientist or any other role or job discription related to my skill set. \n{context}"),
+            ("user","{input}")
+        ]
+    )
+
+    #from langchain.chains.combine_documents import create_stuff_documents_chain
+    document_chain = create_stuff_documents_chain(model,prompt)
+    retriever_chain = create_retrieval_chain(retriever, document_chain)
 
     with st.sidebar:
         pdf_viewer(input=uploaded_file.getvalue(), width=700)
 
     input_text = st.text_input("Ask a question (e.g., Your experience in AI)")
     if input_text:
-        result = vectorstore.similarity_search(input_text)
-        for i, doc in enumerate(result, start=1):
-            st.write(f"🔹 **Document {i}**")
-            st.write(f"📄 **Content:**\n{doc.page_content}\n")
-            st.write("=" * 80)
+        result = retriever_chain.invoke({"input":input_text, "context":"context"})
+        st.write(result['answer'])        
+        #result = vectorstore.similarity_search(input_text)
+    
+        with st.expander("Extra Info"):
+            for i, doc in enumerate(result['context'], start=1):
+                st.write(f"🔹 **Document {i}**")
+                st.write(f"📄 **Content:**\n{doc}\n")
+                st.write("=" * 80)
